838dac643c5bc0994d2545a0fd773612f966af4b
Owner <Owner@USER>
2016-12-14 16:36:09 +0200

    loaded models singletone

--- Game - Client/Loaded_Models.h
+++ Game - Client/Loaded_Models.h
@@ -38,3 +38,25 @@ public:
 };
 
 
+class Loaded_ModelsST
+{
+public:
+	static Loaded_ModelsST& getInstance()
+	{
+		static Loaded_ModelsST Instance;
+		return Instance;
+	}
+	Loaded_ModelsST(Loaded_ModelsST const&) = delete;
+	void operator=(Loaded_ModelsST const&) = delete;
+private:
+	Loaded_ModelsST() {}
+	~Loaded_ModelsST(){}
+	void initialize_Models();
+	Model* operator[](string i);
+	//Loader* getModelData(const string Model_Code);
+	enum Models_Names { e_KatarinaModel, e_MinionModel, e_MapModel, e_SkyBox, e_2D_IMAGE };
+public:
+	static Loaded_ModelsST Instance;
+
+
+};
